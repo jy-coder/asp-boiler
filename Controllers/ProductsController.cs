03fa291d88082079aecb3e7ab8f7e99d5ea9f129
jy-coder <cjyclx@gmail.com>
2023-06-18 23:51:03 +0800

    [UPDATE] Product controller method

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -31,11 +31,30 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProductById(int id)
         {
             return await _productRepository.GetProductByIdAsync(id);
 
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateProduct(int id, [FromBody] ProductUpdateDto updatedProductDto)
+        {
+            var existingProduct = await _productRepository.GetProductByIdAsync(id);
+
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(updatedProductDto, existingProduct);
+
+            if (await _productRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update product");
+        }
+
+
 
     }
 }
