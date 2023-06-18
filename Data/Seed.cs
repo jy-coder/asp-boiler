03fcbc4a45db3000c816fb6ac3d6c451fbfd25d0
jy-coder <cjyclx@gmail.com>
2023-06-18 23:45:34 +0800

    [UPDATE] Add seed data

--- Data/Seed.cs
+++ Data/Seed.cs
@@ -23,13 +23,74 @@ namespace API.Data
                 using var hmac = new HMACSHA512();
 
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("123456"));
                 user.PasswordSalt = hmac.Key;
-
+                foreach (var photo in user.Photos)
+                {
+                    photo.AssociatedEntity = "AppUser";
+                    context.Photos.Add(photo);
+                }
                 context.Users.Add(user);
             }
 
             await context.SaveChangesAsync();
         }
+
+        public static async Task SeedProducts(DataContext context)
+        {
+            if (await context.Products.AnyAsync()) return;
+
+            var productData = await File.ReadAllTextAsync("Data/ProductSeedData.json");
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var products = JsonSerializer.Deserialize<List<Product>>(productData, options);
+
+            foreach (var product in products)
+            {
+                foreach (var photo in product.Photos)
+                {
+                    photo.AssociatedEntity = "Product";
+                    photo.ProductId = product.Id;
+                    context.Photos.Add(photo);
+                }
+
+                var categoryIds = product.Categories.Select(c => c.Id).ToList();
+                product.Categories = context.Categories
+                    .Where(c => categoryIds.Contains(c.Id))
+                    .ToList();
+
+                product.ProductCategories = categoryIds.Select(categoryId => new ProductCategory
+                {
+                    ProductId = product.Id,
+                    CategoryId = categoryId
+                }).ToList();
+
+
+
+                context.Products.Add(product);
+            }
+
+            await context.SaveChangesAsync();
+        }
+
+
+        public static async Task SeedCategories(DataContext context)
+        {
+            if (context.Categories.Any())
+            {
+                return;
+            }
+
+            var categoryData = File.ReadAllText("Data/CategorySeedData.json");
+            var categories = JsonSerializer.Deserialize<List<Category>>(categoryData);
+
+            context.Categories.AddRange(categories);
+            await context.SaveChangesAsync();
+        }
+
+
+
+
     }
 }
