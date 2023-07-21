60e7bd32e9c9f66d0cc751751ffc74549034a15e
jy-coder <cjyclx@gmail.com>
2023-07-21 16:07:43 +0800

    [UPDATE] Reinitialize migration

---
+++ Entities/ProductCategory.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class ProductCategory
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Migrations/20230721072020_AddProduct.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Products_ProductId",
+                table: "Photos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Photos_ProductId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Photos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Photos",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_ProductId",
+                table: "Photos",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Products_ProductId",
+                table: "Photos",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
